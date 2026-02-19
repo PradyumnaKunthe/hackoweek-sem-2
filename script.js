cd14c1dca85e4e17c2535b3628425f01abbc5adb
PradyumnaKunthe <Kunthepraduman@gmail.com>
2026-02-19 11:06:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function filterMenu(category){
-    let cards=document.querySelectorAll(".card");
+document.getElementById("eventForm").addEventListener("submit", function(e){
+    e.preventDefault();
 
-    cards.forEach(card=>{
-        if(category==="all"){
-            card.style.display="block";
-        }
-        else if(card.classList.contains(category)){
-            card.style.display="block";
-        }
-        else{
-            card.style.display="none";
-        }
-    });
-}
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let phone = document.getElementById("phone").value.trim();
+
+    if(name.length < 3){
+        alert("Name must be at least 3 characters");
+        return;
+    }
+
+    if(phone.length !== 10){
+        alert("Enter valid 10 digit phone number");
+        return;
+    }
+
+    document.getElementById("msg").innerText =
+        "🎉 Registration Successful! See you at the event.";
+
+    this.reset();
+});
